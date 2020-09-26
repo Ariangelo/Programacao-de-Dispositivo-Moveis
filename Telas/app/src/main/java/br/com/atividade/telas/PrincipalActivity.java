3d49a5ea01bc47a92bae4883de66e96ebdc1f185
Ariangelo <ariangelo@gmail.com>
2020-09-26 16:22:46 -0300

    Atualização

---
+++ Telas/app/src/main/java/br/com/atividade/telas/PrincipalActivity.java
@@ -0,0 +1,33 @@
+package br.com.atividade.telas;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_inicio,
+                R.id.navigation_dashboard,
+                R.id.navigation_notificacao)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+}
---
+++ Telas/app/src/main/java/br/com/atividade/telas/ui/InicioFragment.java
@@ -0,0 +1,19 @@
+package br.com.atividade.telas.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import br.com.atividade.telas.R;
+
+public class InicioFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.inicio_fragment, container, false);
+        return root;
+    }
+}
